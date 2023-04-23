11441d862f0e6eb7ff90ce8527bee7367d3d9226
ArthurValladares <arthurkaukal@gmail.com>
2023-04-23 14:21:37 -0700

    clear surface function operating on raw pixels

--- SoftwareRendering/main.cpp
+++ SoftwareRendering/main.cpp
@@ -1,27 +1,38 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #include <SDL.h>
-
-// Define MAX and MIN macros
-#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 // Define screen dimensions
 #define SCREEN_WIDTH    800
 #define SCREEN_HEIGHT   600
 
+void clear_surface(SDL_Surface* surface, Uint8 red, Uint8 green, Uint8 blue) {
+    const int width = surface->w;
+    const int height = surface->h;
+    SDL_PixelFormat* pixel_format = surface->format;
+    const int bpp = pixel_format->BytesPerPixel;
+    assert(bpp == 4); // Not supporting non-32-bit pixel formats
+    const Uint32 pixel_color = SDL_MapRGB(pixel_format, red, green, blue);
+
+    for (int h = 0; h < height; ++h) {
+        for (int w = 0; w < width; ++w) {
+            Uint32* p = (Uint32*) ((Uint8*) surface->pixels + (h * surface->pitch) + (w * bpp));
+            *p = pixel_color;
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    // Initialize SDL
+    // Init
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("SDL could not be initialized!\n"
             "SDL_Error: %s\n", SDL_GetError());
         return 0;
     }
-
-    // Create window
     SDL_Window* window = SDL_CreateWindow("Basic C SDL project",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
@@ -31,72 +42,35 @@ int main(int argc, char* argv[])
     {
         printf("Window could not be created!\n"
             "SDL_Error: %s\n", SDL_GetError());
+        return 0;
     }
-    else
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+ 
+    // Render loop
+    bool quit = false;
+    while (!quit)
     {
-        // Create renderer
-        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        if (!renderer)
+        SDL_Event e;
+        SDL_WaitEvent(&e);
+        if (e.type == SDL_QUIT)
         {
-            printf("Renderer could not be created!\n"
-                "SDL_Error: %s\n", SDL_GetError());
+            quit = true;
         }
-        else
-        {
-            // Declare rect of square
-            SDL_Rect squareRect;
-
-            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-            squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-            squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-            // Square position: In the middle of the screen
-            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
-
-            // Event loop exit flag
-            bool quit = false;
-
-            // Event loop
-            while (!quit)
-            {
-                SDL_Event e;
-
-                // Wait indefinitely for the next available event
-                SDL_WaitEvent(&e);
-
-                // User requests quit
-                if (e.type == SDL_QUIT)
-                {
-                    quit = true;
-                }
-
-                // Initialize renderer color white for the background
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-                // Clear screen
-                SDL_RenderClear(renderer);
-
-                // Set renderer color red to draw the square
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-
-                // Draw filled square
-                SDL_RenderFillRect(renderer, &squareRect);
-
-                // Update screen
-                SDL_RenderPresent(renderer);
-            }
-
-            // Destroy renderer
-            SDL_DestroyRenderer(renderer);
+        if (SDL_LockSurface(surface) < 0) {
+            printf("Could not lock SDL surface");
+            return 0;
         }
 
-        // Destroy window
-        SDL_DestroyWindow(window);
+        clear_surface(surface, 255, 0, 0);
+
+        SDL_UnlockSurface(surface);
+
+        SDL_UpdateWindowSurface(window);
     }
 
-    // Quit SDL
+    
+    // Cleanup
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
