1b9aa9f2d58401e79272d109370b851904d02051
ArthurKValladares <arthurkaukal@gmail.com>
2023-09-10 19:30:26 -0300

    more work on macOS setup again

---
+++ macOS/SoftwareRendering/SoftwareRendering/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SoftwareRendering
+//
+//  Created by Arthur Valladares on 9/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
