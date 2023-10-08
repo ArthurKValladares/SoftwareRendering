ee3a63ee7d3a921e7e86428e2b0838185b6e654f
ArthurKValladares <arthurkaukal@gmail.com>
2023-10-08 01:32:54 -0400

    more SIMD work, better struct name

--- macOS/SoftwareRendering/SIMD/integer.cpp
+++ macOS/SoftwareRendering/SIMD/integer.cpp
@@ -1,21 +1,45 @@
 #include "integer.hpp"
 
-SIMDVec4::SIMDVec4(i32 a, i32 b, i32 c, i32 d) {
+Vec4i32::Vec4i32(i32 a, i32 b, i32 c, i32 d) {
     _mi = _mm_set_epi32(a, b, c, d);
 }
 
-SIMDVec4::SIMDVec4(__m128i a) {
+Vec4i32::Vec4i32(__m128i a) {
     _mi = a;
 }
 
-SIMDVec4 SIMDVec4::operator+(const SIMDVec4&rhs) {
-    return SIMDVec4(_mm_add_epi32(_mi, rhs._mi));
+Vec4i32 Vec4i32::operator+(const Vec4i32&rhs) const {
+    return Vec4i32(_mm_add_epi32(_mi, rhs._mi));
+}
+Vec4i32 Vec4i32::operator-(const Vec4i32&rhs) const {
+    return Vec4i32(_mm_sub_epi32(_mi, rhs._mi));
+}
+Vec4i32 Vec4i32::operator*(const Vec4i32& rhs) const {
+    return Vec4i32(_mm_mullo_epi32(_mi, rhs._mi));
 }
 
-SIMDVec4 SIMDVec4::operator-(const SIMDVec4&rhs) {
-    return SIMDVec4(_mm_sub_epi32(_mi, rhs._mi));
+Vec4i32 Vec4i32::operator|(const Vec4i32& rhs) const {
+    return Vec4i32(_mm_or_si128(_mi, rhs._mi));
+}
+Vec4i32 Vec4i32::operator^(const Vec4i32& rhs) const {
+    return Vec4i32(_mm_xor_si128(_mi, rhs._mi));
+}
+Vec4i32 Vec4i32::operator!() const {
+    return *this ^ Vec4i32(0xffff, 0xffff, 0xffff, 0xffff);
 }
 
-SIMDVec4 SIMDVec4::operator*(const SIMDVec4& rhs) {
-    return SIMDVec4(_mm_mullo_epi32(_mi, rhs._mi));
+Vec4i32 Vec4i32::operator==(const Vec4i32& rhs) const {
+    return Vec4i32(_mm_cmpeq_epi32(_mi, rhs._mi));
+}
+Vec4i32 Vec4i32::operator>(const Vec4i32& rhs) const {
+    return Vec4i32(_mm_cmpgt_epi32(_mi, rhs._mi));
+}
+Vec4i32 Vec4i32::operator<(const Vec4i32& rhs) const {
+    return Vec4i32(_mm_cmplt_epi32(_mi, rhs._mi));
+}
+Vec4i32 Vec4i32::operator>=(const Vec4i32& rhs) const {
+    return Vec4i32(_mm_or_si128(_mi, rhs._mi));
+}
+Vec4i32 Vec4i32::operator<=(const Vec4i32& rhs) const {
+    return Vec4i32(_mm_or_si128(_mi, rhs._mi));
 }
--- macOS/SoftwareRendering/SIMD/integer.hpp
+++ macOS/SoftwareRendering/SIMD/integer.hpp
@@ -4,13 +4,24 @@
 #include "defs.h"
 #include "sse2neon.h"
 
-struct SIMDVec4 {
-    SIMDVec4(i32 a, i32 b, i32 c, i32 d);
-    SIMDVec4(__m128i a);
+struct Vec4i32 {
+    Vec4i32(i32 a, i32 b, i32 c, i32 d);
+    Vec4i32(__m128i a);
     
-    SIMDVec4 operator+(const SIMDVec4& rhs);
-    SIMDVec4 operator-(const SIMDVec4& rhs);
-    SIMDVec4 operator*(const SIMDVec4& rhs);
+    Vec4i32 operator+(const Vec4i32& rhs) const;
+    Vec4i32 operator-(const Vec4i32& rhs) const;
+    Vec4i32 operator*(const Vec4i32& rhs) const;
+    
+    Vec4i32 operator|(const Vec4i32& rhs) const;
+    Vec4i32 operator^(const Vec4i32& rhs) const;
+    Vec4i32 operator!() const;
+    
+    // TODO: Some conviniences later to return 4 bools instead
+    Vec4i32 operator==(const Vec4i32& rhs) const;
+    Vec4i32 operator>(const Vec4i32& rhs) const;
+    Vec4i32 operator<(const Vec4i32& rhs) const;
+    Vec4i32 operator>=(const Vec4i32& rhs) const;
+    Vec4i32 operator<=(const Vec4i32& rhs) const;
 private:
     __m128i _mi;
 };
