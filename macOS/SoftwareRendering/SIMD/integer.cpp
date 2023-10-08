32100586717f955ff4fbe9351995a49c69d49b18
ArthurKValladares <arthurkaukal@gmail.com>
2023-10-08 01:01:21 -0400

    some operators for SIMD type

--- macOS/SoftwareRendering/SIMD/integer.cpp
+++ macOS/SoftwareRendering/SIMD/integer.cpp
@@ -1,5 +1,21 @@
 #include "integer.hpp"
 
-i128::i128(i32 a, i32 b, i32 c, i32 d) {
+SIMDVec4::SIMDVec4(i32 a, i32 b, i32 c, i32 d) {
     _mi = _mm_set_epi32(a, b, c, d);
+}
+
+SIMDVec4::SIMDVec4(__m128i a) {
+    _mi = a;
+}
+
+SIMDVec4 SIMDVec4::operator+(const SIMDVec4&rhs) {
+    return SIMDVec4(_mm_add_epi32(_mi, rhs._mi));
+}
+
+SIMDVec4 SIMDVec4::operator-(const SIMDVec4&rhs) {
+    return SIMDVec4(_mm_sub_epi32(_mi, rhs._mi));
+}
+
+SIMDVec4 SIMDVec4::operator*(const SIMDVec4& rhs) {
+    return SIMDVec4(_mm_mullo_epi32(_mi, rhs._mi));
 }
--- macOS/SoftwareRendering/SIMD/integer.hpp
+++ macOS/SoftwareRendering/SIMD/integer.hpp
@@ -4,8 +4,13 @@
 #include "defs.h"
 #include "sse2neon.h"
 
-struct i128 {
-    i128(i32 a, i32 b, i32 c, i32 d);
+struct SIMDVec4 {
+    SIMDVec4(i32 a, i32 b, i32 c, i32 d);
+    SIMDVec4(__m128i a);
+    
+    SIMDVec4 operator+(const SIMDVec4& rhs);
+    SIMDVec4 operator-(const SIMDVec4& rhs);
+    SIMDVec4 operator*(const SIMDVec4& rhs);
 private:
     __m128i _mi;
 };
